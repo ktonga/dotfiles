74a3f90fde72cf239a9aa2c5978ecb1520289824
Gaston Tonietti <gaston.tonietti@gmail.com>
2020-07-02 09:12:49 +1000

    Fish: Local bin + Nix profile

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -4,3 +4,12 @@ set -xg FZF_DEFAULT_COMMAND ag -g \"\"
 set -xg JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
 set -xg TERM xterm-256color
 set -xg TMPDIR /tmp
+
+if test -e ~/.local/bin
+  set -xg PATH ~/.local/bin $PATH
+end
+
+# Nix
+if test -e ~/.nix-profile/etc/profile.d/nix.sh
+  fenv source ~/.nix-profile/etc/profile.d/nix.sh
+end
