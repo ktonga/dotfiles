12a454483aa5608d23dd3fec09444ebb14ede238
Gastón Tonietti <gaston.tonietti@gmail.com>
2022-10-26 09:15:23 +1100

    Fish: Google Cloud SDK

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,6 +7,9 @@ if not set -q MY_FISH_CONFIG
     fenv source ~/.nix-profile/etc/profile.d/nix.sh
   end
 
+  # The next line updates PATH for the Google Cloud SDK.
+  if [ -f '/home/gaston/google-cloud-sdk/path.fish.inc' ]; . '/home/gaston/google-cloud-sdk/path.fish.inc'; end
+
 end
 
 set -xg TERM 'xterm-256color'
